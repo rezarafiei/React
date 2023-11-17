f4fc958b42fe22832db35d4bf3edfd974a2350e4
rezarafiei <rafiei.sr@gmail.com>
2023-11-17 23:52:08 +0330

    why I have error?

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import './index.css'
 
 const showNames = ['reza', 'ashkan', 'parisa']
 
-//const fname = "reza";
 
 const sayHello = <div>
   <ul>
-    {showNames.map(item => <li key={item}>{item}</li>)}
+{/* چرا اینجا ما ارور داریم زمانی که اکولاد میگذاریم */}
+    {
+      showNames.map((element) => {<li key={element}>{element}</li>})
+    }
   </ul>
 </div>
 ReactDOM.createRoot(document.getElementById('root')).render(sayHello)
