82856e103ac102434d6e00785fa8aa158ebae216
rezarafiei <rafiei.sr@gmail.com>
2023-11-17 19:12:12 +0330

    inline css for jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import './index.css'
 const fname = "reza";
 
 const sayHello = <div>
-  <p className='text harchi' id="alaki">HI {fname}</p>
+  <p className='text'>HI <span style={{color:"rgb(0,0,255)",fontSize:"3em",padding:10}}>{fname}</span></p>
   <br />
-  <label htmlFor="fname">First {Fname}</label>
+  <label htmlFor="fname">First name</label>
 </div>
 ReactDOM.createRoot(document.getElementById('root')).render(sayHello)
